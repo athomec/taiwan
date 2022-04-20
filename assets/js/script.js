20554e9e655d0cf4f7946ff156cbb144717ca8c8
vagabond <athomec@pie.com.tw>
2022-04-20 11:10:49 +0800

    首頁新增登入後示意，內頁新增側邊選單點擊後關閉功能

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,60 +3,52 @@ $(function () {//JS開頭
 	var WINDOW = $(window).width();//視窗寬度
 	var WINDOWH = $(window).height();//視窗高度
 	var scrollBottom = $(document).height() - $(window).height() - $(window).scrollTop();
-//---------------------捲動設定---------------------
-$(window).scroll(function () {
-	if ($(this).scrollTop() > 50) { //若目前的位置距離網頁頂端>80px
-		$(".gotop").fadeIn("fast");
-		$("header").addClass("up");
-	} else {
-		$(".gotop").stop().fadeOut("fast");
-		$("header").removeClass("up");
-	}
+	//---------------------捲動設定---------------------
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 50) { //若目前的位置距離網頁頂端>80px
+			$(".gotop").fadeIn("fast");
+			$("header").addClass("up");
+		} else {
+			$(".gotop").stop().fadeOut("fast");
+			$("header").removeClass("up");
+		}
 
-	if ($(this).scrollTop()>80) { 
-		$(".js-side-menu").addClass("fixed");
-	} else {
-		$(".js-side-menu").removeClass("fixed");
-	}
-});
+		if ($(this).scrollTop() > 80) {
+			$(".js-side-menu").addClass("fixed");
+		} else {
+			$(".js-side-menu").removeClass("fixed");
+		}
+	});
 
-//內頁側邊選單設定
-$(".js-page-navbar-toggler").click(function() {
-	$(".js-side-menu").toggleClass("open");
-})
-$(".js-anchor-link").click(function() {
-	let target=$(this).attr('href');
-	$("html,body").animate(
-		{
-		  scrollTop: $(target).offset().top - 120 //直接到相對位置
-		},
-		200
-	  );
-	  return false;
-})
-//---------------------頁籤設定------------------------
-$('.js-check-menu').find(".js-check-button").click(function () {
-	var selectedClassName = $(this).attr('data-target');
-	$(".js-content-item").removeClass("show");
-	$(selectedClassName).addClass("show");
-	$('.js-check-menu').find(".js-check-button").removeClass('active');
-	$(this).addClass("active");
-});
+	//內頁側邊選單設定
+	$(".js-page-navbar-toggler").click(function () {
+		$(".js-side-menu").toggleClass("open");
+	})
 
-$('.js-toggle-menu').find(".js-toggle-button").click(function () {
-	$(this).toggleClass("active");
-	$(this).siblings('.js-toggle-button').removeClass('active');
-	
-});
+	//---------------------頁籤設定------------------------
+	$('.js-check-menu').find(".js-check-button").click(function () {
+		var selectedClassName = $(this).attr('data-target');
+		$(".js-content-item").removeClass("show");
+		$(selectedClassName).addClass("show");
+		$('.js-check-menu').find(".js-check-button").removeClass('active');
+		$(this).addClass("active");
+	});
 
-$('.dropdown').hover(function() {
-	$(this).find(".dropdown-menu").addClass('show');
-},function() {
-	$(this).find(".dropdown-menu").removeClass('show');
-});
+	$('.js-toggle-menu').find(".js-toggle-button").click(function () {
+		$(this).toggleClass("active");
+		$(this).siblings('.js-toggle-button').removeClass('active');
+
+	});
+
+	$('.dropdown').hover(function () {
+		$(this).find(".dropdown-menu").addClass('show');
+	}, function () {
+		$(this).find(".dropdown-menu").removeClass('show');
+	});
+
 
 	//---------------------文字縮放設定----------------------
-	$(".js-font-size").find("a").click(function () {
+	/*$(".js-font-size").find("a").click(function () {
 		$(".js-font-size").find("a").removeClass("now");
 		$(this).addClass("now");
 		if ($(this).attr("data-size") == "txt-sm") {
@@ -67,7 +59,7 @@ $('.dropdown').hover(function() {
 			$("body").css('font-size', '1.1em');
 		}
 		return false;
-	})
+	})*/
 
 
 	//手風琴按鈕切換設定
@@ -81,73 +73,38 @@ $('.dropdown').hover(function() {
 
 	});
 
-	
-	
-	//消息輪播設定
-	$('.flood-slider').slick({
-		dots: true,
-		infinite: false,
-		speed: 300,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive: [
-		  {
-			breakpoint: 1200,
-			settings: {
-			  slidesToShow: 2,
-			  slidesToScroll: 2,
-			  infinite: true,
-			  dots: true
-			}
-		  },
-		  {
-			breakpoint: 768,
-			settings: {
-			  slidesToShow: 1,
-			  slidesToScroll: 1
-			}
-		  }
-		  // You can unslick at a given breakpoint now by adding:
-		  // settings: "unslick"
-		  // instead of a settings object
-		]
-	  });
-
-	  $('.disaster-slider').slick({
-		dots: true,
-		infinite: false,
-		speed: 300,
-		slidesToShow: 3,
-		slidesToScroll: 3,
-		responsive: [
-		  {
-			breakpoint: 1200,
-			settings: {
-			  slidesToShow: 2,
-			  slidesToScroll: 2,
-			  infinite: true,
-			  dots: true
-			}
-		  },
-		  {
-			breakpoint: 768,
-			settings: {
-			  slidesToShow: 1,
-			  slidesToScroll: 1
-			}
-		  }
-		  // You can unslick at a given breakpoint now by adding:
-		  // settings: "unslick"
-		  // instead of a settings object
-		]
-	  });
 
 	RESIZE();
 
-/*	function RESIZE() {
+	$(window).resize(function () {
+		RESIZE();
+	});
+
+	function RESIZE() {
 		WINDOWH = $(window).height();
 		WINDOW = $(window).width();
-	}*/
+		if (WINDOW < 1201) {
+			$(".js-side-menu").find(".js-anchor-link").click(function () {
+				$(".js-page-navbar-toggler").trigger("click");
+			});
+			$(".js-navbar-nav").find(".js-anchor-link").click(function () {
+				$(".js-navbar-toggler").trigger("click");
+			});
+		}
+	}
+	function anchor() {
+		$(".js-anchor-link").click(function () {
+			let target = $(this).attr('href');
+			$("html,body").animate(
+				{
+					scrollTop: $(target).offset().top - 120 //直接到相對位置
+				},
+				200
+			);
+			return false;
+		});
+	}
+	
 
 
 })//JS尾端	
